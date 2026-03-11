22ef06bfeb2b3d35bc8580ab3d853d09c8374979
lakshayinfo9871-wq <lakshayinfo9871@gmail.com>
2026-03-11 12:57:47 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -63,7 +63,7 @@ self.addEventListener('fetch',e=>{
 self.addEventListener('push',e=>{
   let data={title:'BSC Store',body:'New notification',url:'/',tag:'bsc-push'};
   if(e.data){try{Object.assign(data,JSON.parse(e.data.text()));}catch{data.body=e.data.text();}}
-  e.waitUntil(self.registration.showNotification(data.title,{body:data.body,icon:'/icon-192.png',badge:'/icon-192.png',tag:data.tag,data:{url:data.url||'/'},vibrate:[200,100,200],actions:[{action:'open',title:'🛒 Open Store'},{action:'dismiss',title:'Dismiss'}]}));
+  e.waitUntil(self.registration.showNotification(data.title,{body:data.body,icon:'/icons/icon-192.png',badge:'/icons/icon-192.png',tag:data.tag,data:{url:data.url||'/'},vibrate:[200,100,200],actions:[{action:'open',title:'🛒 Open Store'},{action:'dismiss',title:'Dismiss'}]}));
 });
 
 self.addEventListener('notificationclick',e=>{
